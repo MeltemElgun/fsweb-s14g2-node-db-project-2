9c145d27146cce2f19a410cf9293c90770616619
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-03-29 04:27:21 +0300

    fsweb-s14g2-node-db-project-2 -4Test kaldı.

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,76 @@
-const checkCarId = (req, res, next) => {
+const carsModel = require("./cars-model");
+const vinValidator = require("vin-validator");
+
+const checkCarId = async (req, res, next) => {
   // HOKUS POKUS
-}
+  try {
+    let { id } = req.params;
+    const isExistCar = await carsModel.getById(id);
+    if (!isExistCar) {
+      res
+        .status(404)
+        .json({ message: `${id} kimliğine sahip araba bulunamadı` });
+    } else {
+      req.currentCar = isExistCar;
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
   // HOKUS POKUS
-}
+  try {
+    let fields = ["vin", "make", "model", "mileage"];
+    let missedFields = [];
+    for (let i = 0; i < fields; i++) {
+      if (req.body[fields[i]] === undefined) {
+        missedFields.push(fields[i]);
+      }
+    }
+    if (missedFields.length > 0) {
+      res
+        .status(400)
+        .json({ message: `${missedFields.toString()} is missing.` });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 const checkVinNumberValid = (req, res, next) => {
   // HOKUS POKUS
-}
+  try {
+    const isValidVin = vinValidator.validate(req.body.vin);
+    if (!isValidVin) {
+      res.status(400).json({ message: `vin ${req.body.vin} is invalid` });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
-const checkVinNumberUnique = (req, res, next) => {
+const checkVinNumberUnique = async (req, res, next) => {
   // HOKUS POKUS
-}
+  try {
+    const isExistVinNumber = await carsModel.getByVinNumber(req.body.vin);
+    if (isExistVinNumber) {
+      res.status(400).json({ message: `vin ${req.body.vin} already exists` });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+};
--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,27 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
   // HOKUS POKUS
-}
+  return db("cars");
+};
 
-const getById = () => {
+const getById = (id) => {
   // HOKUS POKUS
-}
+  return db("cars").where({ id }).first();
+};
 
-const create = () => {
+const getByVinNumber = (vin) => {
+  return db("cars").where({ vin }).first();
+};
+const create = async (cars) => {
   // HOKUS POKUS
-}
+  let [id] = await db("cars").insert(cars);
+  return getById(id);
+};
+
+module.exports = {
+  getAll,
+  getById,
+  getByVinNumber,
+  create,
+};
